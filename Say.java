9a186a8008a1f3911881dfc89e0d9ff57935a700
nsm <821649672@qq.com>
2018-01-19 12:43:49 +0800

    Hello World!

---
+++ Say.java
@@ -0,0 +1,25 @@
+public class Say{
+	public static void main(String []args){
+		System.out.println("              ,,,,,,");
+		System.out.println("          o#'9MMHb':'-,o,");
+		System.out.println("       .oH\":HH$' \"' ' -*R&o,");
+		System.out.println("      dMMM*\"\"'`'      .oM\"HM?.");
+		System.out.println("    ,MMM'          \"HLbd< ?&H\\");
+		System.out.println("   .:MH .\"\\          ` MM  MM&b");
+		System.out.println("  . \"*H    -        &MMMMMMMMMH:");
+		System.out.println("  .    dboo        MMMMMMMMMMMM.");
+		System.out.println("  .   dMMMMMMb      *MMMMMMMMMP.");
+		System.out.println("  .    MMMMMMMP        *MMMMMP .");
+		System.out.println("       `#MMMMM           MM6P ,");
+		System.out.println("   '    `MMMP\"           HM*`,");
+		System.out.println("    '    :MM             .- ,");
+		System.out.println("     '.   `#?..  .       ..'");
+		System.out.println("        -.   .         .-");
+		System.out.println("          ''-.oo,oo.-''");
+        System.out.println();
+        System.out.print("           ");
+		for(int i=0;i<args.length;++i){
+			System.out.print(args[i]+" ");
+		}
+	}
+}
